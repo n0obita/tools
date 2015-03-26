d90e327e6eb3a61594e8ed3fb8784378f7203326
n0obita <recto.jose.f.santos.jr@outlook.com>
2015-03-26 17:24:01 +0800

    Removed unneccessary spaces

---
+++ Spammer/Spammer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "C:\\hello\\world";
+        string newText = path.Substring(path.LastIndexOf('\\') + 1);
+
+
+        Console.WriteLine(newText);
+
+        Console.WriteLine("Done!");
+        Console.ReadLine();
+    }
+}
